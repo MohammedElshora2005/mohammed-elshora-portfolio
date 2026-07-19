b82dc7e1bf387718e3778cbb0a4783d0a3c8e215
MohammedElshora2005 <muhammedhosni70@gmail.com>
2026-07-20 02:58:04 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,13 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild', // ✅ بدل terser عشان مش محتاج تثبيت
     rollupOptions: {
       output: {
         manualChunks: {
