e673e030546b14320428ace88c8a18686d00da5a
MohammedElshora2005 <muhammedhosni70@gmail.com>
2026-07-25 16:11:55 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     open: true,
@@ -19,7 +20,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'esbuild', // ✅ بدل terser عشان مش محتاج تثبيت
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
